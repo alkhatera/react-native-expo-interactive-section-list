32ed01f599fd24a4fccd0a47152d53c1ffa893e5
Alaa Alkhater <38783431+3la2ooh@users.noreply.github.com>
2022-05-24 14:42:05 +0300

    enhancements

--- src/InteractiveSectionList.tsx
+++ src/InteractiveSectionList.tsx
@@ -1,13 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import {
-  View,
-  SectionList,
-  Text,
-  StyleSheet,
-  ImageSourcePropType,
-  SectionListRenderItem,
-  SectionListData,
-} from 'react-native';
+import { View, SectionList, Text, StyleSheet, ImageSourcePropType, SectionListRenderItem, SectionListData } from 'react-native';
 import sectionListGetItemLayout from 'react-native-section-list-get-item-layout';
 
 import ScrollableTabbar from './ScrollableTabbar';
@@ -15,21 +7,24 @@ import ScrollableTabbar from './ScrollableTabbar';
 import { contentViewStyles as styles } from './styles';
 
 interface Props {
-  data: Array<any>;
-  renderItem: SectionListRenderItem<any>;
-  itemHeight: number;
-  renderSectionHeader?: (info: {
-    section: SectionListData<any>;
-  }) => JSX.Element;
-  tabbarItemWidth?: number;
-  tabbarItemHeight?: number;
-  tabbarItemSpaceBetween?: number;
-  tabbarItemActiveColor?: string;
-  tabbarItemInactiveColor?: string;
-  tabbarItemTitleActiveColor?: string;
-  tabbarItemTitleInactiveColor?: string;
-  tabbarItemFontSize?: number;
-  tabbarIcon?: ImageSourcePropType;
+	data: Array<any>;
+	renderItem: SectionListRenderItem<any>;
+	renderSeparatorComponent: () => JSX.Element;
+	separatorHeight: number;
+	itemHeight: number;
+	renderSectionHeader?: (info: { section: SectionListData<any> }) => JSX.Element;
+	sectionHeaderHeight?: number;
+	tabbarItemWidth?: number;
+	tabbarItemHeight?: number;
+	tabbarItemSpaceBetween?: number;
+	tabbarItemActiveColor?: string;
+	tabbarItemInactiveColor?: string;
+	tabbarItemTitleActiveColor?: string;
+	tabbarItemTitleInactiveColor?: string;
+	tabbarItemFontSize?: number;
+	tabbarIcon?: JSX.Element;
+	isSearchBarShown?: boolean;
+	searchBar?: JSX.Element;
 }
 
 // DATA IS SORTED ALPHABETICALL
@@ -38,202 +33,191 @@ interface Props {
 // LISTITEM AND A SECOND FLATTENDED LIST IS
 // CREATED (FOR COMPARISSON)
 const getData = (items: Array<any>) => {
-  const flattenedList: Array<any> = [];
+	const flattenedList: Array<any> = [];
 
-  items.sort((a, b) => {
-    if (a.title > b.title) {
-      return 1;
-    }
+	items.sort((a, b) => {
+		if (a.title > b.title) {
+			return 1;
+		}
 
-    return -1;
-  });
+		return -1;
+	});
 
-  const sectionList: Array<any> = items.map((section, index) => {
-    section.data.forEach((sectionItem: any) => {
-      if (typeof sectionItem === 'object') {
-        sectionItem.sectionIndex = index;
-        flattenedList.push(sectionItem);
-      } else if (typeof sectionItem === 'string') {
-        const obj = { title: sectionItem, sectionIndex: index };
-        flattenedList.push(obj);
-      }
-    });
+	const sectionList: Array<any> = items.map((section, index) => {
+		section.data.forEach((sectionItem: any) => {
+			if (typeof sectionItem === 'object') {
+				sectionItem.sectionIndex = index;
+				flattenedList.push(sectionItem);
+			} else if (typeof sectionItem === 'string') {
+				const obj = { title: sectionItem, sectionIndex: index };
+				flattenedList.push(obj);
+			}
+		});
 
-    return section;
-  });
+		return section;
+	});
 
-  return { sectionList, flattenedList };
+	return { sectionList, flattenedList };
 };
 
 // TAB ITEMS CREATED FROM SECTION DATA ADDED TO SECTIONLIST
 const getTabbarItems = (items: Array<any>) => {
-  const tabbarItems: Array<string> = [];
+	const tabbarItems: Array<string> = [];
 
-  items.forEach((item: any) => {
-    if (!tabbarItems.some((tabbarItem) => tabbarItem === item.title)) {
-      tabbarItems.push(item.title);
-    }
-  });
+	items.forEach((item: any) => {
+		if (!tabbarItems.some((tabbarItem) => tabbarItem === item.title)) {
+			tabbarItems.push(item.title);
+		}
+	});
 
-  return tabbarItems;
+	return tabbarItems;
 };
 
 let scrollOffset = 0;
 
 const InteractiveSectionList: React.FC<Props> = ({
-  data,
-  renderItem,
-  itemHeight,
-  renderSectionHeader,
-  tabbarItemWidth,
-  tabbarItemHeight,
-  tabbarItemSpaceBetween,
-  tabbarItemActiveColor,
-  tabbarItemInactiveColor,
-  tabbarItemTitleActiveColor,
-  tabbarItemTitleInactiveColor,
-  tabbarItemFontSize,
-  tabbarIcon,
+	data,
+	renderItem,
+	itemHeight,
+	renderSectionHeader,
+	renderSeparatorComponent,
+	separatorHeight,
+	sectionHeaderHeight = 0,
+	tabbarItemWidth,
+	tabbarItemHeight,
+	tabbarItemSpaceBetween,
+	tabbarItemActiveColor,
+	tabbarItemInactiveColor,
+	tabbarItemTitleActiveColor,
+	tabbarItemTitleInactiveColor,
+	tabbarItemFontSize,
+	tabbarIcon,
+	isSearchBarShown,
+	searchBar,
 }) => {
-  const flatListRef = useRef<SectionList>(null);
+	const flatListRef = useRef<SectionList>(null);
 
-  const [sectionsFeed, setSectionsFeed] = useState<Array<any>>([]);
-  const [flattenedList, setFlattenedList] = useState<Array<any>>([]);
+	const [sectionsFeed, setSectionsFeed] = useState<Array<any>>([]);
+	const [flattenedList, setFlattenedList] = useState<Array<any>>([]);
 
-  const [tabbarItems, setTabbarItems] = useState<Array<string>>([]);
+	const [tabbarItems, setTabbarItems] = useState<Array<string>>([]);
 
-  const [selectedIndex, setSelectedIndex] = useState<number>(0);
-  const [firstIndexInView, setFirstIndexInView] = useState<number>(0);
-  const [isManualSelect, setIsManualSelect] = useState<boolean>(false);
-  const [scrollDirection, setScrollDirection] = useState<string>('DOWN');
+	const [selectedIndex, setSelectedIndex] = useState<number>(0);
+	const [firstIndexInView, setFirstIndexInView] = useState<number>(0);
+	const [isManualSelect, setIsManualSelect] = useState<boolean>(false);
+	const [scrollDirection, setScrollDirection] = useState<string>('DOWN');
 
-  const [sectionHeaderHeight, setSectionHeaderHeight] = useState<number>(40);
-  const [layoutHeight, setLayoutHeight] = useState<number>(0);
+	const [layoutHeight, setLayoutHeight] = useState<number>(0);
 
-  const handleTabItemPress = (index: number) => {
-    setSelectedIndex(index);
-    setIsManualSelect(true);
-  };
+	const handleTabItemPress = (index: number) => {
+		setSelectedIndex(index);
+		setIsManualSelect(true);
+	};
 
-  const getItemLayout = sectionListGetItemLayout({
-    getItemHeight: () => itemHeight,
-    getSeparatorHeight: () => StyleSheet.hairlineWidth,
-    getSectionHeaderHeight: () => sectionHeaderHeight,
-    getSectionFooterHeight: () => 0,
-    listHeaderHeight: 0,
-  });
+	const getItemLayout = sectionListGetItemLayout({
+		getItemHeight: () => itemHeight,
+		getSeparatorHeight: () => separatorHeight,
+		getSectionHeaderHeight: () => sectionHeaderHeight,
+		getSectionFooterHeight: () => 0,
+		listHeaderHeight: 0,
+	});
 
-  const handleRenderSectionHeader: any = (info: {
-    section: SectionListData<any>;
-  }) => {
-    if (renderSectionHeader) {
-      return renderSectionHeader(info);
-    }
+	const handleRenderSectionHeader: any = (info: { section: SectionListData<any> }) => {
+		if (renderSectionHeader) {
+			return renderSectionHeader(info);
+		}
 
-    return (
-      <Text
-        style={styles.sectionHeader}
-        onLayout={({ nativeEvent }) =>
-          setSectionHeaderHeight(nativeEvent.layout.height)
-        }
-      >
-        {info.section.title}
-      </Text>
-    );
-  };
+		return <Text style={styles.sectionHeader}>{info.section.title}</Text>;
+	};
 
-  useEffect(() => {
-    if (!isManualSelect) return;
+	useEffect(() => {
+		if (!isManualSelect) return;
 
-    if (flatListRef && flatListRef.current) {
-      flatListRef.current.scrollToLocation({
-        animated: true,
-        itemIndex: 0,
-        sectionIndex: selectedIndex,
-        viewPosition: 0,
-      });
-    }
-  }, [selectedIndex]);
+		if (flatListRef && flatListRef.current) {
+			flatListRef.current.scrollToLocation({
+				animated: true,
+				itemIndex: 0,
+				sectionIndex: selectedIndex,
+				viewPosition: 0,
+			});
+		}
+	}, [selectedIndex]);
 
-  useEffect(() => {
-    const listObject = getData(data);
+	useEffect(() => {
+		const listObject = getData(data);
 
-    setSectionsFeed(listObject.sectionList);
-    setFlattenedList(listObject.flattenedList);
-    setTabbarItems(getTabbarItems(listObject.sectionList));
-  }, [data]);
+		setSectionsFeed(listObject.sectionList);
+		setFlattenedList(listObject.flattenedList);
+		setTabbarItems(getTabbarItems(listObject.sectionList));
+	}, [data]);
 
-  const renderFlatlist = () => (
-    <SectionList
-      ref={flatListRef}
-      contentContainerStyle={styles.contentContainer}
-      sections={sectionsFeed}
-      renderItem={renderItem}
-      getItemLayout={(data: any, index: number) => getItemLayout(data, index)}
-      renderSectionHeader={handleRenderSectionHeader}
-      scrollEventThrottle={16}
-      onScroll={({ nativeEvent }) => {
-        if (layoutHeight === 0) {
-          setLayoutHeight(nativeEvent.layoutMeasurement.height);
-        }
+	const renderFlatlist = () => (
+		<SectionList
+			ref={flatListRef}
+			contentContainerStyle={styles.contentContainer}
+			sections={sectionsFeed}
+			renderItem={renderItem}
+			getItemLayout={(data: any, index: number) => getItemLayout(data, index)}
+			renderSectionHeader={handleRenderSectionHeader}
+			scrollEventThrottle={16}
+			onScroll={({ nativeEvent }) => {
+				if (layoutHeight === 0) {
+					setLayoutHeight(nativeEvent.layoutMeasurement.height);
+				}
 
-        //  IDENTIFY SECTION IN VIEW
-        const itemIndex = Math.floor(
-          nativeEvent.contentOffset.y /
-            (itemHeight +
-              ((sectionHeaderHeight + StyleSheet.hairlineWidth) *
-                sectionsFeed.length) /
-                flattenedList.length)
-        );
+				//  IDENTIFY SECTION IN VIEW
+				const itemIndex = Math.floor(
+					nativeEvent.contentOffset.y / (itemHeight + ((sectionHeaderHeight + separatorHeight) * sectionsFeed.length) / flattenedList.length)
+				);
 
-        // EXIT IF SCROLL OUT OF BOUNDS
-        if (itemIndex < 0 || itemIndex > flattenedList.length - 1) return;
+				// EXIT IF SCROLL OUT OF BOUNDS
+				if (itemIndex < 0 || itemIndex > flattenedList.length - 1) return;
 
-        // SET FIRST SECTION IN VIEW (TOP)
-        const sectionIndex = flattenedList[itemIndex].sectionIndex;
-        setFirstIndexInView(sectionIndex);
+				// SET FIRST SECTION IN VIEW (TOP)
+				const sectionIndex = flattenedList[itemIndex].sectionIndex;
+				setFirstIndexInView(sectionIndex);
 
-        // SET SCROLLDIRECTION
-        if (nativeEvent.contentOffset.y > scrollOffset) {
-          setScrollDirection('DOWN');
-        } else {
-          setScrollDirection('UP');
-        }
-        scrollOffset = nativeEvent.contentOffset.y;
-      }}
-      onScrollBeginDrag={() => {
-        setSelectedIndex(-1);
-        setIsManualSelect(false);
-      }}
-      ItemSeparatorComponent={() => <View style={styles.border} />}
-      keyExtractor={(item, index) => index.toString()}
-    />
-  );
+				// SET SCROLLDIRECTION
+				if (nativeEvent.contentOffset.y > scrollOffset) {
+					setScrollDirection('DOWN');
+				} else {
+					setScrollDirection('UP');
+				}
+				scrollOffset = nativeEvent.contentOffset.y;
+			}}
+			onScrollBeginDrag={() => {
+				setSelectedIndex(-1);
+				setIsManualSelect(false);
+			}}
+			ItemSeparatorComponent={renderSeparatorComponent}
+			keyExtractor={(item, index) => index.toString()}
+		/>
+	);
 
-  return (
-    <>
-      <ScrollableTabbar
-        items={tabbarItems}
-        onPress={(index: number) => handleTabItemPress(index)}
-        selectedIndex={selectedIndex}
-        firstValueInView={
-          data[firstIndexInView] ? data[firstIndexInView].title : ''
-        }
-        isManualSelect={isManualSelect}
-        scrollDirection={scrollDirection}
-        itemWidth={tabbarItemWidth}
-        itemHeight={tabbarItemHeight}
-        itemSpaceBetween={tabbarItemSpaceBetween}
-        activeColor={tabbarItemActiveColor}
-        inactiveColor={tabbarItemInactiveColor}
-        titleActiveColor={tabbarItemTitleActiveColor}
-        titleInactiveColor={tabbarItemTitleInactiveColor}
-        fontSize={tabbarItemFontSize}
-        icon={tabbarIcon}
-      />
-      {React.useMemo(() => renderFlatlist(), [sectionsFeed])}
-    </>
-  );
+	return (
+		<>
+			<ScrollableTabbar
+				items={tabbarItems}
+				onPress={(index: number) => handleTabItemPress(index)}
+				selectedIndex={selectedIndex}
+				firstValueInView={data[firstIndexInView] ? data[firstIndexInView].title : ''}
+				isManualSelect={isManualSelect}
+				scrollDirection={scrollDirection}
+				itemWidth={tabbarItemWidth}
+				itemHeight={tabbarItemHeight}
+				itemSpaceBetween={tabbarItemSpaceBetween}
+				activeColor={tabbarItemActiveColor}
+				inactiveColor={tabbarItemInactiveColor}
+				titleActiveColor={tabbarItemTitleActiveColor}
+				titleInactiveColor={tabbarItemTitleInactiveColor}
+				fontSize={tabbarItemFontSize}
+				icon={tabbarIcon}
+				isSearchBarShown={isSearchBarShown}
+				searchBar={searchBar}
+			/>
+			{React.useMemo(() => renderFlatlist(), [sectionsFeed])}
+		</>
+	);
 };
 
 export default InteractiveSectionList;
--- src/TabbarItem.tsx
+++ src/TabbarItem.tsx
@@ -1,89 +1,76 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  GestureResponderEvent,
-  LayoutChangeEvent,
-} from 'react-native';
+import { View, Text, TouchableOpacity, GestureResponderEvent, LayoutChangeEvent } from 'react-native';
 
 import { tabbarItemStyles as styles } from './styles';
 
 export interface Props {
-  item: string;
-  onPress: (event: GestureResponderEvent) => void;
-  onLayout: (event: LayoutChangeEvent) => void;
-  selected: boolean;
-  isFirstInView: boolean;
-  isManualSelect: boolean;
-  itemWidth?: number;
-  itemHeight?: number;
-  activeColor?: string;
-  inactiveColor?: string;
-  titleActiveColor?: string;
-  titleInactiveColor?: string;
-  fontSize?: number;
+	item: string;
+	onPress: (event: GestureResponderEvent) => void;
+	onLayout: (event: LayoutChangeEvent) => void;
+	selected: boolean;
+	isFirstInView: boolean;
+	isManualSelect: boolean;
+	itemWidth?: number;
+	itemHeight?: number;
+	activeColor?: string;
+	inactiveColor?: string;
+	titleActiveColor?: string;
+	titleInactiveColor?: string;
+	fontSize?: number;
 }
 
 const TabbarItem: React.FC<Props> = ({
-  item,
-  onPress,
-  onLayout,
-  selected,
-  isFirstInView,
-  isManualSelect,
-  itemWidth,
-  itemHeight,
-  activeColor,
-  inactiveColor,
-  titleActiveColor,
-  titleInactiveColor,
-  fontSize,
+	item,
+	onPress,
+	onLayout,
+	selected,
+	isFirstInView,
+	isManualSelect,
+	itemWidth,
+	itemHeight,
+	activeColor,
+	inactiveColor,
+	titleActiveColor,
+	titleInactiveColor,
+	fontSize,
 }) => {
-  return (
-    <TouchableOpacity
-      onLayout={onLayout}
-      style={[
-        styles.container,
-        {
-          width: itemWidth,
-          height: itemHeight,
-        },
-        ,
-      ]}
-      onPress={onPress}
-      activeOpacity={0.8}
-    >
-      <View
-        style={[
-          styles.innerContainer,
-          {
-            backgroundColor:
-              (isManualSelect && selected) || (!isManualSelect && isFirstInView)
-                ? activeColor
-                : inactiveColor,
-          },
-        ]}
-      >
-        <Text
-          style={[
-            styles.itemTitle,
-            { fontSize },
-            {
-              color:
-                (isManualSelect && selected) ||
-                (!isManualSelect && isFirstInView)
-                  ? titleActiveColor
-                  : titleInactiveColor,
-            },
-          ]}
-          numberOfLines={1}
-        >
-          {item}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity
+			onLayout={onLayout}
+			style={[
+				styles.container,
+				{
+					width: itemWidth,
+					height: itemHeight,
+				},
+				,
+			]}
+			onPress={onPress}
+			activeOpacity={0.8}
+		>
+			<View
+				style={[
+					styles.innerContainer,
+					{
+						backgroundColor: (isManualSelect && selected) || (!isManualSelect && isFirstInView) ? activeColor : inactiveColor,
+					},
+				]}
+			>
+				<Text
+					style={[
+						styles.itemTitle,
+						{ fontSize },
+						{
+							color: (isManualSelect && selected) || (!isManualSelect && isFirstInView) ? titleActiveColor : titleInactiveColor,
+						},
+					]}
+					numberOfLines={1}
+				>
+					{item}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 export default TabbarItem;
